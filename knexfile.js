07e5638536eced58c3a8df1f7ecaf4bdff16b576
kuberdhure <kuberdhureofficial@gmail.com>
2024-04-22 01:49:20 +0530

    my commit

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const path = require("path");
+
+// if (process.env.USER) require("dotenv").config();
+
+const {
+  DATABASE_URL = "postgres://cxcyygny:HK5sm9ATZGhzT_mWi0xKbhHF9GEzwJDw@kashin.db.elephantsql.com/cxcyygny"
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
